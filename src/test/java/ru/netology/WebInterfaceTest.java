e16427fda3c6ab0342fad2ee78f2ecda371a79d5
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-03-23 21:02:51 +0300

    added tests on the fields name and phone

--- src/test/java/ru/netology/WebInterfaceTest.java
+++ src/test/java/ru/netology/WebInterfaceTest.java
@@ -18,4 +18,95 @@ public class WebInterfaceTest {
 
     }
 
+    @Test
+    void shouldEmptyFieldName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79008007755");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldInvalidFieldName1() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Evgeny Mironov");
+        $("[data-test-id=phone] input").setValue("+79008007755");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldInvalidFieldName2() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Иван_Иванов");
+        $("[data-test-id=phone] input").setValue("+79008007755");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldInvalidFieldName3() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("+79008007755");
+        $("[data-test-id=phone] input").setValue("+79008007755");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+    }
+
+    @Test
+    void shouldEmptyFieldPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Антон Васильев");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone]").find("[class=input__sub]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldInvalidFieldPhone1() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Антон Васильев");
+        $("[data-test-id=phone] input").setValue("Антон Васильев");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone]").find("[class=input__sub]").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    void shouldInvalidFieldPhone2() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Антон Васильев");
+        $("[data-test-id=phone] input").setValue("+7(900)800-55-99");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone]").find("[class=input__sub]").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+//        $(".input_invalid").find("[class=input__sub]").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldAllFieldsEmpty() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[class=input__sub]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $(".input_invalid").find("[class=input__sub]").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+    }
+
 }
+
+
